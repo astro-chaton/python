1eee620ac3aa6689bc3720ff2d96d22e21335b89
astro-chaton <86916894+astro-chaton@users.noreply.github.com>
2021-09-06 15:17:20 +0200

    Create stepik 7.9.1.py

---
+++ stepik 7.9.1.py
@@ -0,0 +1,14 @@
+import os
+import sys
+os.system("cls")
+
+
+n = int(input())
+c = 1
+k = 1
+for _ in range(n)
+    for _ in range(c)
+        print(k, end=' ')
+        k = k + 1
+    c = c + 1
+    print()
